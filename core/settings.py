5df4c47439d3129d3a89902e550e39d6a60865e1
Madat Bay <medet.bayramov22@gmail.com>
2021-07-27 00:10:32 +0400

    graphql graphene installed

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
     'blog',
 ]
 
@@ -124,3 +125,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPHENE = {
+    "SCHEMA": "blog.schema.schema"
+}
