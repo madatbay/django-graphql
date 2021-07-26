b7ec3489cd73e1e0796b022cfe8a1ceeb6a683eb
Madat Bay <medet.bayramov22@gmail.com>
2021-07-27 00:36:16 +0400

    simple graphql schema created

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,7 @@ class Category(models.Model):
 class Article(models.Model):
     name = models.CharField(max_length=100)
     body = models.TextField()
-    category = models.ForeignKey(Category, related_name="categories", on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name="articles", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- blog/schema.py
+++ blog/schema.py
@@ -1,6 +1,30 @@
 import graphene
+from graphene_django import DjangoObjectType, fields
+
+from blog.models import Article, Category
+
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'articles')
+
+class ArticleType(DjangoObjectType):
+    class Meta:
+        model = Article
+        fields = ('id', 'name', 'body', 'category')
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hi!")
+    all_articles = graphene.List(ArticleType)
+    category_by_name = graphene.Field(CategoryType, name=graphene.String(required=True))
 
+    def resolve_all_articles(root, info):
+        return Article.objects.select_related('category').all()
+
+    def resolve_category_by_name(root, info, name):
+        try:
+            return Category.objects.get(name=name)
+        except Category.DoesNotExist:
+            return None
+            
 schema = graphene.Schema(query=Query)
