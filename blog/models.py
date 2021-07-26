7fbcd998bc803953017a7b4525822f17ef6d46c7
Madat Bay <medet.bayramov22@gmail.com>
2021-07-27 00:20:51 +0400

    blog models added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    name = models.CharField(max_length=100)
+    body = models.TextField()
+    category = models.ForeignKey(Category, related_name="categories", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
